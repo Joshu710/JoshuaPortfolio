3dcbb24de34cf649fbe29d03dd97107c55272796
Joshua Bernal <joshuabernal710@gmail.com>
2024-02-03 16:27:21 -0500

    Edit vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/joshuabernal/",
+  base: "/JoshuaPortfolio/",
   plugins: [react()],
 });
